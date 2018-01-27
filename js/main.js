1be22a5558e5db560a82c440756533a921d9085b
wakeboardhq <brian@wakeboardhq.com>
2018-01-26 21:02:02 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,200 +1,97 @@
-/*
-	Phantom by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+$(document).ready(function() {
 
-(function($) {
 
-	skel.breakpoints({
-		xlarge:	'(max-width: 1680px)',
-		large:	'(max-width: 1280px)',
-		medium:	'(max-width: 980px)',
-		small:	'(max-width: 736px)',
-		xsmall:	'(max-width: 480px)'
-	});
+	// Scroll Events
+	$(window).scroll(function(){
 
-	$(function() {
+		var wScroll = $(this).scrollTop();
 
-		var	$window = $(window),
-			$body = $('body');
+		// Activate menu
+		if (wScroll > 20) {
+			$('#main-nav').addClass('active');
+			$('#slide_out_menu').addClass('scrolled');
+		}
+		else {
+			$('#main-nav').removeClass('active');
+			$('#slide_out_menu').removeClass('scrolled');
+		};
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
 
-			$window.on('load', function() {
-				window.setTimeout(function() {
-					$body.removeClass('is-loading');
-				}, 100);
-			});
-
-		// Touch?
-			if (skel.vars.mobile)
-				$body.addClass('is-touch');
-
-		// Forms.
-			var $form = $('form');
-
-			// Auto-resizing textareas.
-				$form.find('textarea').each(function() {
-
-					var $this = $(this),
-						$wrapper = $('<div class="textarea-wrapper"></div>'),
-						$submits = $this.find('input[type="submit"]');
-
-					$this
-						.wrap($wrapper)
-						.attr('rows', 1)
-						.css('overflow', 'hidden')
-						.css('resize', 'none')
-						.on('keydown', function(event) {
-
-							if (event.keyCode == 13
-							&&	event.ctrlKey) {
-
-								event.preventDefault();
-								event.stopPropagation();
-
-								$(this).blur();
-
-							}
-
-						})
-						.on('blur focus', function() {
-							$this.val($.trim($this.val()));
-						})
-						.on('input blur focus --init', function() {
-
-							$wrapper
-								.css('height', $this.height());
-
-							$this
-								.css('height', 'auto')
-								.css('height', $this.prop('scrollHeight') + 'px');
-
-						})
-						.on('keyup', function(event) {
-
-							if (event.keyCode == 9)
-								$this
-									.select();
-
-						})
-						.triggerHandler('--init');
-
-					// Fix.
-						if (skel.vars.browser == 'ie'
-						||	skel.vars.mobile)
-							$this
-								.css('max-height', '10em')
-								.css('overflow-y', 'auto');
-
-				});
-
-			// Fix: Placeholder polyfill.
-				$form.placeholder();
-
-		// Prioritize "important" elements on medium.
-			skel.on('+medium -medium', function() {
-				$.prioritize(
-					'.important\\28 medium\\29',
-					skel.breakpoint('medium').active
-				);
-			});
-
-		// Menu.
-			var $menu = $('#menu');
-
-			$menu.wrapInner('<div class="inner"></div>');
-
-			$menu._locked = false;
-
-			$menu._lock = function() {
-
-				if ($menu._locked)
-					return false;
-
-				$menu._locked = true;
-
-				window.setTimeout(function() {
-					$menu._locked = false;
-				}, 350);
-
-				return true;
-
-			};
-
-			$menu._show = function() {
-
-				if ($menu._lock())
-					$body.addClass('is-menu-visible');
-
-			};
-
-			$menu._hide = function() {
-
-				if ($menu._lock())
-					$body.removeClass('is-menu-visible');
-
-			};
-
-			$menu._toggle = function() {
-
-				if ($menu._lock())
-					$body.toggleClass('is-menu-visible');
-
-			};
-
-			$menu
-				.appendTo($body)
-				.on('click', function(event) {
-					event.stopPropagation();
-				})
-				.on('click', 'a', function(event) {
-
-					var href = $(this).attr('href');
-
-					event.preventDefault();
-					event.stopPropagation();
-
-					// Hide.
-						$menu._hide();
-
-					// Redirect.
-						if (href == '#menu')
-							return;
-
-						window.setTimeout(function() {
-							window.location.href = href;
-						}, 350);
-
-				})
-				.append('<a class="close" href="#menu">Close</a>');
-
-			$body
-				.on('click', 'a[href="#menu"]', function(event) {
-
-					event.stopPropagation();
-					event.preventDefault();
-
-					// Toggle.
-						$menu._toggle();
-
-				})
-				.on('click', function(event) {
-
-					// Hide.
-						$menu._hide();
-
-				})
-				.on('keydown', function(event) {
-
-					// Hide on escape.
-						if (event.keyCode == 27)
-							$menu._hide();
-
-				});
+		//Scroll Effects
 
 	});
 
-})(jQuery);
+
+	// Navigation
+	$('#navigation').on('click', function(e){
+		e.preventDefault();
+		$(this).addClass('open');
+		$('#slide_out_menu').toggleClass('open');
+
+		if ($('#slide_out_menu').hasClass('open')) {
+			$('.menu-close').on('click', function(e){
+				e.preventDefault();
+				$('#slide_out_menu').removeClass('open');
+			})
+		}
+	});
+
+
+	// Price Table
+	var individual_price_table = $('#price_tables').find('.individual');
+	var company_price_table = $('#price_tables').find('.company');
+
+
+	$('.switch-toggles').find('.individual').addClass('active');
+	$('#price_tables').find('.individual').addClass('active');
+
+	$('.switch-toggles').find('.individual').on('click', function(){
+		$(this).addClass('active');
+		$(this).closest('.switch-toggles').removeClass('active');
+		$(this).siblings().removeClass('active');
+		individual_price_table.addClass('active');
+		company_price_table.removeClass('active');
+	});
+
+	$('.switch-toggles').find('.company').on('click', function(){
+		$(this).addClass('active');
+		$(this).closest('.switch-toggles').addClass('active');
+		$(this).siblings().removeClass('active');
+		company_price_table.addClass('active');
+		individual_price_table.removeClass('active');			
+	});
+
+
+	// Wow Animations
+    wow = new WOW(
+      {
+      boxClass:     'wow',      // default
+      animateClass: 'animated', // default
+      offset:       0,          // default
+      mobile:       true,       // default
+      live:         true        // default
+    }
+    )
+    wow.init();
+
+
+    // Menu For Xs Mobile Screens
+    if ($(window).height() < 450) {
+    	$('#slide_out_menu').addClass('xs-screen');
+    }
+
+    $(window).on('resize', function(){
+	    if ($(window).height() < 450) {
+	    	$('#slide_out_menu').addClass('xs-screen');
+	    } else{
+	    	$('#slide_out_menu').removeClass('xs-screen');
+	    }
+    });
+
+
+    // Magnific Popup
+    $(".lightbox").magnificPopup();
+
+
+
+});
